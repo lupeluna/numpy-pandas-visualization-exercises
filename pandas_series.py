c665a3cbe6a8cd1a67c61a90ab4e0191b74eae73
Lupe Luna <luna.guadalupe898@gmail.com>
2021-04-18 21:18:19 -0500

    pandas series

---
+++ pandas_series.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Exercises Part I
+
+# Use Series attributes and methods to explore your fruits 
+# Series.
+
+# 1.  Determine the number of elements in fruits.
+
+
+
+# 2.  Output only the index from fruits.
+
+
+
+# 3.  Output only the values from fruits.
+
+
+# 4.  Confirm the data type of the values in fruits.
+
+
+# 5.  Output only the first five values from fruits. Output the last three values. 
+#  Output two random values from fruits.
+
+
+
+# 6. Run the .describe() on fruits to see what information it returns when called on a 
+#  Series with string values.
+
+
+# 7.  Run the code necessary to produce only the unique string values from fruits.
+
+
+# 8.  Determine how many times each unique string value occurs in fruits.
+
+
+# 9.  Determine the string value that occurs most frequently in fruits.
+
+
+# 10.  Determine the string value that occurs least frequently in fruits.
